c7e1e40eb5a656cb289b47dad62d1ea835540f82
Damian Slomian <damian.slomian@mgr-is.de>
2025-11-17 14:14:25 +0100

    Test

--- src/main/java/de/websso/MeController.java
+++ src/main/java/de/websso/MeController.java
@@ -1,22 +1,22 @@
 package de.websso;
 
 import java.util.Map;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.oauth2.core.oidc.user.OidcUser;
+//import org.springframework.security.core.annotation.AuthenticationPrincipal;
+//import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MeController {
 
-    @GetMapping("/me")
-    public Map<String, Object> me(@AuthenticationPrincipal OidcUser user) {
-        System.out.println("=== AUTHORITIES ===");
-        user.getAuthorities().forEach(a -> System.out.println(" - " + a.getAuthority()));
-
-        System.out.println("=== CLAIMS ===");
-        user.getClaims().forEach((k, v) -> System.out.println(k + " = " + v));
-
-        return user.getClaims();
-    }
+//    @GetMapping("/me")
+//    public Map<String, Object> me(@AuthenticationPrincipal OidcUser user) {
+//        System.out.println("=== AUTHORITIES ===");
+//        user.getAuthorities().forEach(a -> System.out.println(" - " + a.getAuthority()));
+//
+//        System.out.println("=== CLAIMS ===");
+//        user.getClaims().forEach((k, v) -> System.out.println(k + " = " + v));
+//
+//        return user.getClaims();
+//    }
 }
